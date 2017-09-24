625f85729dbcabdb7fbddcc5fa24c194fe5302b1
marknazareno <mark.nazareno@yahoo.com>
2017-09-24 15:47:19 +0800

    add Docket config for customizing the API title and description

--- hello-app-web/src/main/java/com/mnazareno/helloapp/config/HelloAppConfig.java
+++ hello-app-web/src/main/java/com/mnazareno/helloapp/config/HelloAppConfig.java
@@ -8,6 +8,10 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
@@ -26,5 +30,14 @@ public class HelloAppConfig extends WebMvcConfigurerAdapter {
     @Bean
     public PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer() {
         return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Hello App").description("REST API using Spring MVC").build();
     }
 }
